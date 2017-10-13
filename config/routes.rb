4e60c0f708d34f45a955657366e4610090772b27
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-10-13 10:43:51 +0200

    Added new files, stocks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
-  get 'welcome/index'
+  get 'welcome/index', to: 'welcome#index'
+
+  # Custom route
+  devise_scope :user do get '/login' => 'devise/sessions#new' end
   root 'welcome#index'
 end

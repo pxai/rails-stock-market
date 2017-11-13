c01b6010850470e333cb1281a5098d9390ff85a9
Pello Altadill <pello_altadill@cuatrovientos.org>
2017-11-13 11:28:42 +0100

    Added show method in user controller 192

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,11 @@ class UsersController < ApplicationController
     @friendships = current_user.friends
   end
 
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.user_stocks
+  end
+  
   def search
     @users = User.search(params[:search_param])
     if @users
